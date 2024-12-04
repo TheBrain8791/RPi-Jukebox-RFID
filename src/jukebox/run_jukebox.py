87b1cd5c318adba9348373280577815235b6e5f4
TheBrain1978 <newsletter.gb@liwest.at>
2024-12-04 17:39:35 +0100

    jukebox enable pyhton debugging

--- src/jukebox/run_jukebox.py
+++ src/jukebox/run_jukebox.py
@@ -17,7 +17,11 @@ import logging
 import logging.config
 import jukebox.daemon
 import misc.loggingext
+import debugpy
 
+debugpy.listen(("0.0.0.0", 5678))
+print("Waiting for client to attach...")
+debugpy.wait_for_client()
 
 def main():
     # Get absolute path of this script
